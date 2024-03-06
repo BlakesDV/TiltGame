919fb8adcb1b916dbcbefac16aacae17284366b4
BlakesDV <dmunoz@cocoschool.mx>
2024-03-05 22:14:35 -0600

    Art & Canvas Imp
    Implementation of basic art, canvas and imported TMP essentials

---
+++ TiltCar/Assets/Scripts/SceneMngr.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneMngr : MonoBehaviour
+{
+    private void Start()
+    {
+        Time.timeScale = 0; //pausa el juego incluyendo las animaciones
+    }
+
+    public void ClosePanel()
+    {
+        GetComponent<Animator>().SetTrigger("SceneChange");
+    }
+
+    public void SetTimePlay()
+    {
+        Time.timeScale = 1;
+    }
+    public void OpenCredits()
+    {
+        GetComponent<Animator>().SetTrigger("CreditsScene");
+    }
+    public void ExitCreditsScene()
+    {
+        GetComponent<Animator>().SetTrigger("ExitCreditsScene");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+        print("exit");
+    }
+}
